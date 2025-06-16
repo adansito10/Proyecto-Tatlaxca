de5bfda7b6a683bdd36f9feb3cd0ba78c4216807
adansito10 <adandejesus200420@gmail.com>
2025-06-16 17:49:39 -0600

    login y cerrar sesion integrados

--- src/app/layout/navigation/navigation.component.ts
+++ src/app/layout/navigation/navigation.component.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import { Router } from '@angular/router'; // Importamos Router
 
 @Component({
   selector: 'app-navigation',
@@ -10,7 +11,8 @@ import { map, shareReplay } from 'rxjs/operators';
   standalone: false
 })
 export class NavigationComponent {
- private breakpointObserver = inject(BreakpointObserver);
+  private breakpointObserver = inject(BreakpointObserver);
+  private router = inject(Router); // Inyectamos el Router
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
@@ -18,8 +20,8 @@ export class NavigationComponent {
       shareReplay()
     );
 
-  isSideMode = false; 
-  sidenavOpened = true; 
+  isSideMode = false;
+  sidenavOpened = true;
 
   constructor() {
     this.isHandset$.subscribe(isHandset => {
@@ -31,11 +33,19 @@ export class NavigationComponent {
   onSidenavToggle(opened: boolean) {
     this.sidenavOpened = opened;
   }
+
   onLinkClick(drawer: any) {
-  this.isHandset$.subscribe(isHandset => {
-    if (isHandset) {
-      drawer.close();
-    }
-  }).unsubscribe(); // Cerramos la subscripción inmediatamente
-}
+    this.isHandset$.subscribe(isHandset => {
+      if (isHandset) {
+        drawer.close();
+      }
+    }).unsubscribe(); // Cerramos la subscripción inmediatamente
+  }
+
+  logout() {
+    // Borra datos de sesión
+    localStorage.clear(); // O sessionStorage.clear();
+    // Redirige al login
+    this.router.navigate(['/auth/login']);
+  }
 }
